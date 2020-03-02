bf2c3af31e8fb0bf210181d5e53a668e86b16ec2
Matteo Pergolesi <matpergo@gmail.com>
2020-03-02 18:10:59 +0100

    Rename application.

--- src/main/java/it/cnit/nfvoservice/SiteInventoryApplication.java
+++ src/main/java/it/cnit/nfvoservice/SiteInventoryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NfvoserviceApplication {
+public class SiteInventoryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NfvoserviceApplication.class, args);
+        SpringApplication.run(SiteInventoryApplication.class, args);
     }
 
 }
--- src/test/java/it/cnit/nfvoservice/SiteInventoryApplicationTests.java
+++ src/test/java/it/cnit/nfvoservice/SiteInventoryApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NfvoserviceApplicationTests {
+class SiteInventoryApplicationTests {
 
     @Test
     void contextLoads() {

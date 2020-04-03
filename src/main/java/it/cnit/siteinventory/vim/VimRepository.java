e54f3a81a4b451404ca497d934fa072a4be042f0
Matteo Pergolesi <matpergo@gmail.com>
2020-04-03 14:46:27 +0200

    Add findByTypeIgnoreCase to Vim.

--- src/main/java/it/cnit/siteinventory/vim/VimRepository.java
+++ src/main/java/it/cnit/siteinventory/vim/VimRepository.java
@@ -1,8 +1,12 @@
 package it.cnit.siteinventory.vim;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "vims", path = "vims")
 public interface VimRepository extends PagingAndSortingRepository<Vim, Long> {
+    List<Vim> findByTypeIgnoreCase(@Param("type") String type);
 }

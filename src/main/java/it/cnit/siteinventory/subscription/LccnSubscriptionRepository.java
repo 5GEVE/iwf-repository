268b5444a1b9bc0be6fc948cc14ca8af8570fb51
Matteo Pergolesi <matpergo@gmail.com>
2020-04-03 14:49:00 +0200

    Remove unused method.

--- src/main/java/it/cnit/siteinventory/subscription/LccnSubscriptionRepository.java
+++ src/main/java/it/cnit/siteinventory/subscription/LccnSubscriptionRepository.java
@@ -1,13 +1,9 @@
 package it.cnit.siteinventory.subscription;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-import java.util.List;
 
 @SuppressWarnings("unused")
 @RepositoryRestResource(collectionResourceRel = "subscriptions", path = "subscriptions")
 public interface LccnSubscriptionRepository extends PagingAndSortingRepository<LccnSubscription, Long> {
-    List<LccnSubscription> findByNfvo(@Param("nfvo") int nfvo);
 }

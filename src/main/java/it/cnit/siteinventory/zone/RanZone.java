7385e061860881c91e63f2ce83662c28ccde5ea9
Matteo Pergolesi <matpergo@gmail.com>
2020-04-21 15:30:59 +0200

    Add some capabilities to the RAN zone.

--- src/main/java/it/cnit/siteinventory/zone/RanZone.java
+++ src/main/java/it/cnit/siteinventory/zone/RanZone.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -24,8 +25,20 @@ public class RanZone {
 
     private double longitude;
 
+    private double radius;
+
+    // 4G, 5G
+    @Min(4)
+    private int generation;
+
+    // Ex. R16
+    private String release;
+
+    // comma-separated list of strings
+    private String frequencies;
+
     @ManyToOne
     private RanOrchestrator rano;
 
-    // TODO add RAN capabilities
+    // TODO add more RAN capabilities
 }

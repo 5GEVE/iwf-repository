bed8c9a32c76c19327c1a3454f7b0fe493bb0ec3
Matteo Pergolesi <matpergo@gmail.com>
2020-05-13 16:34:28 +0200

    Commeting vim accounts that should be dynamic.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -20,29 +20,27 @@ VALUES ('osm-madrid', 'OSM', 'R6', 'ENABLED', (SELECT id FROM site WHERE name='S
 INSERT INTO nfv_orchestrator (name, type, version, operational_state, site_id)
 VALUES ('osm-athens-wings', 'OSM', 'R6', 'ENABLED', (SELECT id FROM site WHERE name='GREECE_ATHENS'));
 
--- vim
-INSERT INTO vim_account (name, tenant, type, uri, vim_account_nfvo_id)
-VALUES ('Cloud VIM Openstack', '5geve-cloud', 'openstack',
-        'http://10.20.20.1:5000/v3', '77c531ef-ff08-42a3-aeb9-3b8eae19a32f');
-INSERT INTO vim_account (name, tenant, type, uri, vim_account_nfvo_id)
-VALUES ('Edge VIM Openstack', '5geve-edge', 'openstack',
-        'http://10.20.20.1:5000/v3', 'e59a8dfd-dd21-42cc-98f7-a68c83cfbdbd');
+-- vim accounts. Keeping this as a reference. Should be added dynamically.
+-- INSERT INTO vim_account (name, tenant, type, uri, vim_account_nfvo_id)
+-- VALUES ('Cloud VIM Openstack', '5geve-cloud', 'openstack',
+--         'http://10.20.20.1:5000/v3', '77c531ef-ff08-42a3-aeb9-3b8eae19a32f');
+-- INSERT INTO vim_account (name, tenant, type, uri, vim_account_nfvo_id)
+-- VALUES ('Edge VIM Openstack', '5geve-edge', 'openstack',
+--         'http://10.20.20.1:5000/v3', 'e59a8dfd-dd21-42cc-98f7-a68c83cfbdbd');
 
--- vim_nfvo
-INSERT INTO vim_nfvo (vim_id, nfvo_id) VALUES
-((SELECT id FROM vim_account WHERE tenant='5geve-cloud'),
- (SELECT id FROM nfv_orchestrator WHERE name='osm-turin-cnit'));
-INSERT INTO vim_nfvo (vim_id, nfvo_id) VALUES
-((SELECT id FROM vim_account WHERE tenant='5geve-edge'),
- (SELECT id FROM nfv_orchestrator WHERE name='osm-turin-cnit'));
+-- vim_nfvo. Keeping this as a reference. Should be added dynamically.
+-- INSERT INTO vim_nfvo (vim_id, nfvo_id) VALUES
+-- ((SELECT id FROM vim_account WHERE tenant='5geve-cloud'),
+--  (SELECT id FROM nfv_orchestrator WHERE name='osm-turin-cnit'));
+-- INSERT INTO vim_nfvo (vim_id, nfvo_id) VALUES
+-- ((SELECT id FROM vim_account WHERE tenant='5geve-edge'),
+--  (SELECT id FROM nfv_orchestrator WHERE name='osm-turin-cnit'));
 
 -- availability_zone
-INSERT INTO availability_zone (latitude, location, longitude, name, vim_account_id)
-VALUES (45.062379, 'CLOUD', 7.662097, 'polito cloud zone',
-        (SELECT id FROM vim_account WHERE vim_account_nfvo_id='77c531ef-ff08-42a3-aeb9-3b8eae19a32f'));
-INSERT INTO availability_zone (latitude, location, longitude, name, vim_account_id)
-VALUES (45.062207, 'EDGE', 7.678558, 'porta nuova edge zone',
-        (SELECT id FROM vim_account WHERE vim_account_nfvo_id='e59a8dfd-dd21-42cc-98f7-a68c83cfbdbd'));
+INSERT INTO availability_zone (latitude, location, longitude, name)
+VALUES (45.062379, 'CLOUD', 7.662097, 'polito cloud zone');
+INSERT INTO availability_zone (latitude, location, longitude, name)
+VALUES (45.062207, 'EDGE', 7.678558, 'porta nuova edge zone');
 
 -- notification_type initialization
 INSERT INTO notification_type (type) VALUES ('NsLcmOperationOccurenceNotification');

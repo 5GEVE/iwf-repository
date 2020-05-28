6b6b94b40296dfbf27722f3da5e48f6c8b3281df
Matteo Pergolesi <matpergo@gmail.com>
2020-05-28 11:03:57 +0200

    Generate many shippers with some random data.

--- scripts/add-dataShippers-dummies.sh
+++ scripts/add-dataShippers-dummies.sh
@@ -1,26 +1,41 @@
 #!/usr/bin/env bash
 
+# This needs jq, uuidgen (from e2fsprogs)
+
 HOST=localhost
 PORT=8087
 SITE_ID=1 # Italy default
+N=15 # Number of shippers to be generated
 
-SITES_LINK=$(curl --request POST \
-  --url http://${HOST}:${PORT}/dataShippers \
-  --header 'content-type: application/json' \
-  --data '{
-	"dataShipperId": "5bbbe24f-0d75-4044-ad34-2370cf4816ea",
-	"ipAddress": "192.168.17.3",
-	"username": "root",
-	"password": "password",
-	"configStart": "START",
-	"configStop": "STOP",
-	"configReset":"RESET",
-	"method": "SCRIPT",
-	"deviceId": "5g-antenna-asdf",
-	"metricType": "CPU_CONSUMPTION"
-}' | jq -r ._links.site.href)
+generate_post_data()
+{
+  cat <<EOF
+{
+  "dataShipperId": "$(uuidgen)",
+  "ipAddress": "$(printf "%d.%d.%d.%d\n" "$((RANDOM % 256))" "$((RANDOM % 256))" "$((RANDOM % 256))" "$((RANDOM % 256))")",
+  "username": "root",
+  "password": "password",
+  "configStart": "START",
+  "configStop": "STOP",
+  "configReset":"RESET",
+  "method": "SCRIPT",
+  "deviceId": "topic/device/kafka",
+  "metricType": "CPU_CONSUMPTION"
+}
+EOF
+}
 
-curl --request PUT \
-  --url "${SITES_LINK}" \
-  --header 'content-type: text/uri-list' \
-  --data http://${HOST}:${PORT}/sites/"${SITE_ID}"
+for ((i=1;i<=N;i++));
+do
+  printf "generating shipper %s\n" "${i}"
+  SITES_LINK=$(curl --request POST \
+    --url http://${HOST}:${PORT}/dataShippers \
+    --header 'content-type: application/json' \
+    --data "$(generate_post_data)" | jq -r ._links.site.href);
+
+  curl --request PUT \
+    --url "${SITES_LINK}" \
+    --header 'content-type: text/uri-list' \
+    --data http://${HOST}:${PORT}/sites/"${SITE_ID}";
+done
+

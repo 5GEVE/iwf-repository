35ec5185e255a70b38fdd26640ec95c610235ffa
Matteo Pergolesi <matpergo@gmail.com>
2020-06-09 11:31:45 +0200

    Rename application.

--- src/main/java/it/cnit/iwf_repository/IWFRepositoryApplication.java
+++ src/main/java/it/cnit/iwf_repository/IWFRepositoryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SiteInventoryApplication {
+public class IWFRepositoryApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(SiteInventoryApplication.class, args);
+    SpringApplication.run(IWFRepositoryApplication.class, args);
   }
 
 }

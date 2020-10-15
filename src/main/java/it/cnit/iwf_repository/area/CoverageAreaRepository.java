faccaa0c4c1b2261ec2b6262bcd37b5cf6a0e77c
Matteo Pergolesi <matpergo@gmail.com>
2020-10-15 20:51:24 +0200

    Add custom search method to get coverage areas from site name.

--- src/main/java/it/cnit/iwf_repository/area/CoverageAreaRepository.java
+++ src/main/java/it/cnit/iwf_repository/area/CoverageAreaRepository.java
@@ -1,6 +1,9 @@
 package it.cnit.iwf_repository.area;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -8,5 +11,9 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(collectionResourceRel = "coverageAreas", path = "coverageAreas")
 @Tag(name = "Radio Coverage Areas")
 public interface CoverageAreaRepository extends PagingAndSortingRepository<CoverageArea, Long> {
+
+  @Query(nativeQuery = true, value = "select ca.* from site join ran_orchestrator ro join coverage_area ca "
+      + "on ro.id = ca.ran_orchestrator_id on site.id = ro.site_id where site.name=:name")
+  Optional<List<CoverageArea>> findBySite(String name);
 
 }

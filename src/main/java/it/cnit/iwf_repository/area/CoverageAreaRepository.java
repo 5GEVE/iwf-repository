283019b9f965f9557826b12c1dd7c401ef9b0168
Matteo Pergolesi <matpergo@gmail.com>
2020-10-16 11:05:24 +0200

    Remove optional from custom search methods. Add @Param to return better errors.

--- src/main/java/it/cnit/iwf_repository/area/CoverageAreaRepository.java
+++ src/main/java/it/cnit/iwf_repository/area/CoverageAreaRepository.java
@@ -2,9 +2,9 @@ package it.cnit.iwf_repository.area;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @SuppressWarnings("unused")
@@ -12,14 +12,14 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @Tag(name = "Radio Coverage Areas")
 public interface CoverageAreaRepository extends PagingAndSortingRepository<CoverageArea, Long> {
 
-  @Query(nativeQuery = true, value = "select ca.* from site join ran_orchestrator ro join coverage_area ca "
-      + "on ro.id = ca.ran_orchestrator_id on site.id = ro.site_id where site.name=:name")
-  Optional<List<CoverageArea>> findBySiteName(String name);
+  @Query(nativeQuery = true, value =
+      "select ca.* from site join ran_orchestrator ro join coverage_area ca "
+          + "on ro.id = ca.ran_orchestrator_id on site.id = ro.site_id where site.name = :name")
+  List<CoverageArea> findBySiteName(@Param("name") String name);
 
-  @Query(nativeQuery = true, value = "select ca.* from site join ran_orchestrator ro join coverage_area ca "
-      + "on ro.id = ca.ran_orchestrator_id on site.id = ro.site_id where site.id=:id")
-  Optional<List<CoverageArea>> findBySiteId(long id);
-
-  // TODO add find by tech
+  @Query(nativeQuery = true, value =
+      "select ca.* from site join ran_orchestrator ro join coverage_area ca "
+          + "on ro.id = ca.ran_orchestrator_id on site.id = ro.site_id where site.id = :id")
+  List<CoverageArea> findBySiteId(@Param("id") long id);
 
 }

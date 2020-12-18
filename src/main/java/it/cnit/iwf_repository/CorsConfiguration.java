786123e3824b2c86ec3f77cd9e571e6663bf1b40
Matteo Pergolesi <matpergo@gmail.com>
2020-12-18 09:26:16 +0100

    Enable CORS headers.

---
+++ src/main/java/it/cnit/iwf_repository/CorsConfiguration.java
@@ -0,0 +1,16 @@
+package it.cnit.iwf_repository;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**");
+  }
+
+}
